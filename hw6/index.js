71c74478f655f465f79fb2994b440dccb1786379
rnick98 <rnick099@gmail.com>
2021-01-08 12:24:07 +0200

    Home work '6'improvements

--- hw6/index.js
+++ hw6/index.js
@@ -1,34 +1,42 @@
 const symbolInput = prompt('Enter one of these symbols: + - * /');
 
-if (!(symbolInput === '+' || symbolInput === '-' || symbolInput === '*' || symbolInput === '/')) {
+if (!isMathOperation(symbolInput)) {
     alert('Sorry, try again with valid symbol!');
-}
-else {
+} else {
     const numberInput = prompt('Enter a few numbers with spaces between each:');
 
-    let userNumbers = numberInput.split(' ');
-    let numberArray = userNumbers.map(item => parseInt(item));
+    const userNumbers = numberInput.split(' ');
+    
+    const numberArray = userNumbers.map((item) => parseFloat(item));
 
     const filteredNumbers = numberArray.filter(function (item) {
-        if (isNaN(item)) {
-            return false;
-        } else {
-            return true;
-        }
+        return !isNaN(item)
     });
 
-    let numberCalculation = filteredNumbers.reduce(function (previousValue, value) {
-        if (symbolInput === '+') {
-            return previousValue + value;
-        } else if (symbolInput === '-') {
-            return previousValue - value;
-        } else if (symbolInput === '*') {
-            return previousValue * value;
-        } else if (symbolInput === '/') {
-            return previousValue / value;
-        }
-    });
-    alert(`Awesome, your result is ${numberCalculation}!`);
+    if (filteredNumbers.length === 0) {
+        alert('Please, type in valid numbers! Try again')
+    } else {
+        const numberCalculation = calculation(filteredNumbers);
+        alert(`Awesome, your result is ${numberCalculation}!`);
+    }
 };
 
+function calculation(filteredNumbers) {
+    return filteredNumbers.reduce(function (previousValue, value) {
+        switch (symbolInput) {
+            case '+':
+                return previousValue + value;
+            case '-':
+                return previousValue - value;
+            case '*':
+                return previousValue * value;
+            case '/':
+                return previousValue / value;
+        }
+    });
+}
+
+function isMathOperation() {
+    return symbolInput === '+' || symbolInput === '-' || symbolInput === '*' || symbolInput === '/'
+}
 
