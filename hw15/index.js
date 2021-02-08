b6d7e023709de661668191c51d4c9d05f3f67efd
rnick98 <rnick099@gmail.com>
2021-02-08 21:04:15 +0200

    Second commit

--- hw15/index.js
+++ hw15/index.js
@@ -5,25 +5,48 @@ fetch('https://jsonplaceholder.typicode.com/albums')
     .then((albomInfo) => {
         const leftDiv = document.getElementById('left-column');
         const listContainer = document.createElement('ul');
-        const newItem = document.createElement('li');
         const titleList = [];
+
         albomInfo.map(item => {
+            const newItem = document.createElement('li');
             if (item.title) {
+                newItem.setAttribute("id", item.id)
                 newItem.innerHTML = item.title;
+                titleList.push(newItem.innerHTML);
+                listContainer.appendChild(newItem);
             }
-            titleList.push(newItem.innerHTML)
 
+            newItem.addEventListener("click", showAlbomPhoto);
+
+            function showAlbomPhoto() {
+                const albumId = item.id;
+                fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((albomPhotos) => {
+                        const rightDiv = document.getElementById('right-column');
+                        const photoContainer = document.createElement('ul');
+                        const photoList = [];
+
+                        albomPhotos.map(item => {
+                            const newPhotoItem = document.createElement('li');
+                            const image = document.createElement('img');
+
+                            if (item.url) {
+                                image.src = item.url;
+                                image.style = "height: 200px";
+                                newPhotoItem.append(image);
+                                photoList.push(newPhotoItem.innerHTML);
+                                photoContainer.appendChild(newPhotoItem);
+                                rightDiv.appendChild(photoContainer);
+                            }
+                        })
+                    })
+            }
         })
-        console.log(titleList)
-
-        listContainer.appendChild(newItem);
         leftDiv.appendChild(listContainer);
-
-        newItem.textContent = titleList;
-
-
     })
-
 
 .catch(() => {
     console.log('ERROR');
